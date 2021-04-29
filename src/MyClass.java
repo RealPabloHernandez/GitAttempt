904ca973bd2308ba3f813f9684596d006df024f6
RealPabloHernandez <74122319+RealPabloHernandez@users.noreply.github.com>
2021-04-28 20:10:15 -0500

    Message function added

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,5 +1,9 @@
 public class MyClass {
     public static void main(String[] str){
-        System.out.println("Hello");
+        System.out.println(message(true));
+    }
+
+    public static String message(Boolean confirmation){
+        return("Hello dude");
     }
 }
