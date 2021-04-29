c0540002fef76d2f26555cdf24fac682fdf09158
RealPabloHernandez <74122319+RealPabloHernandez@users.noreply.github.com>
2021-04-28 20:30:49 -0500

    Number function added

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,10 +1,13 @@
 public class MyClass {
     public static void main(String[] str){
         System.out.println(message(true));
-        System.out.println("4x4x4");
     }
 
     public static String message(Boolean confirmation){
-        return("Hello dude");
+        return("Hello dude "+number());
+    }
+
+    public static int number(){
+        return (5);
     }
 }
