542e58811c772d7c511c677670b4ea8559ea231e
RealPabloHernandez <74122319+RealPabloHernandez@users.noreply.github.com>
2021-04-28 20:21:32 -0500

    Message added

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,6 +1,7 @@
 public class MyClass {
     public static void main(String[] str){
         System.out.println(message(true));
+        System.out.println("4x4x4");
     }
 
     public static String message(Boolean confirmation){
